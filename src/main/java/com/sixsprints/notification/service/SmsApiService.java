51c38fb7950021b122e1be1e9bffeef1b8562f39
aarvy05 <115387759+yogeshaarvy@users.noreply.github.com>
2022-11-04 11:29:33 +0530

    remove some

--- src/main/java/com/sixsprints/notification/service/SmsApiService.java
+++
@@ -1,9 +0,0 @@
-package com.sixsprints.notification.service;
-
-import retrofit2.Call;
-
-public interface SmsApiService {
-
-  Call<String> sendSms(String apiKey, String senderId, String to, String message, String templateId, int extra);
-
-}
